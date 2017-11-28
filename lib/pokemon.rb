3782eab303b68d4003b98b4d3610a0f65415ef8d
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-28 04:06:40 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -16,10 +16,10 @@ class Pokemon
   end
 
   def self.find(id, db)
-    pokemon = db.execute("SELECT * FROM pokemon WHERE pokemon.id = ?", id)
+    pokemon = db.execute("SELECT * FROM pokemon WHERE pokemon.id = ?", id).flatten
     binding.pry
-    db.execute("UPDATE pokemon SET hp = 60 WHERE id = ?", id) if pokemon[0][3] == nil
-    Pokemon.new(id:id, name: "#{pokemon[0][1]}", type: "#{pokemon[0][2]}", db: db, hp: pokemon[0][3])
+    db.execute("UPDATE pokemon SET hp = 60 WHERE id = ?", id) if pokemon[3] == nil && pokemon.length == 4
+    Pokemon.new(id:id, name: "#{pokemon[1]}", type: "#{pokemon[2]}", db: db, hp: pokemon[3])
   end
 
   def alter_hp(hp, db)
